98a79e71a48876de54dbe1a219973dea8a3e84ea
AkshayJadahv-1922 <aksjadhav35@gmail.com>
2024-01-30 13:03:58 +0530

    Added and configured DB Context

---
+++ AJStore/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AJStore.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+            
+        }
+    }
+}
