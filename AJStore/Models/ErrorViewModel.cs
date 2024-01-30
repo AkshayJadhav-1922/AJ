b36e7e8e383b8f8659ce81e94d88828d82a13aa5
AkshayJadahv-1922 <aksjadhav35@gmail.com>
2024-01-30 12:15:33 +0530

    Add project files.

---
+++ AJStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AJStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
