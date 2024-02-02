d4eb4ad7055c93c434f62668f4d58e592bb39cd3
AkshayJadahv-1922 <aksjadhav35@gmail.com>
2024-02-02 12:55:06 +0530

    Added IRepository and Repository.cs as a first step of implementing Repository pattern

---
+++ Aj.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aj.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class  //Remeber: <T> where T : class - written to make interface generic. 
+    {
+        IEnumerable<T> GetAll();
+        T Get(Expression<Func<T,bool>>filter); //Remeber: Expression<Func<T,bool>>filter which is written to accept arrow function
+        void Add(T entity);
+        void Remove(T entity);
+        void RemoveRaneg(IEnumerable<T> entities);
+
+    }
+}
---
+++ Aj.DataAccess/Repository/IRepository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using Aj.DataAccess.Repository.IRepository;
+using AJ.DataAcess.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Aj.DataAccess.Repository.IRepository
+{
+    internal class Repository<T> : IRepository<T> where T : class  //Remember: Make class generic and observe inheritance
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+
+        public Repository(ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet = _db.Set<T>();
+        }
+        public void Add(T entity)
+        {
+            dbSet.Add(entity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = dbSet.Where(filter);
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRaneg(IEnumerable<T> entities)
+        {
+            dbSet.RemoveRange(entities);
+        }
+    }
+}
