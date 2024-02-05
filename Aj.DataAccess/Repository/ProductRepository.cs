469206867c90f4c33e8f0c2befa215e31df0f3a6
AkshayJadahv-1922 <aksjadhav35@gmail.com>
2024-02-06 00:58:29 +0530

    Uploaded Images for Products, Also written custome logic for updating product

--- Aj.DataAccess/Repository/ProductRepository.cs
+++ Aj.DataAccess/Repository/ProductRepository.cs
@@ -24,7 +24,23 @@ namespace Aj.DataAccess.Repository
 
         public void Update(Product product)
         {
-            _db.Update(product);
+            var objFromDb = _db.Products.FirstOrDefault(u => u.Id == product.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Title = product.Title;
+                objFromDb.Description = product.Description;
+                objFromDb.Price = product.Price;
+                objFromDb.ListPrice = product.ListPrice;
+                objFromDb.Price50 = product.Price50;
+                objFromDb.Price100 = product.Price100;
+                objFromDb.CategoryId = product.CategoryId;
+                objFromDb.Author = product.Author;
+                objFromDb.ISBN = product.ISBN;
+                if (product.ImageUrl != null)
+                {
+                    objFromDb.ImageUrl = product.ImageUrl;
+                }
+            }
         }
 
     }
