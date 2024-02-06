d997eb3c2edce3d19390863738f1521e0b79914f
AkshayJadahv-1922 <aksjadhav35@gmail.com>
2024-02-06 19:41:50 +0530

    Added Asp net core Identity

--- AJStore/Program.cs
+++ AJStore/Program.cs
@@ -3,6 +3,7 @@ using Aj.DataAccess.Repository.IRepository;
 using Aj.DataAccess.Service;
 using AJ.DataAcess.Data;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,8 @@ builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddDbContext<ApplicationDbContext>(option =>
 option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
 
 var app = builder.Build();
 
